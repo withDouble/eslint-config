88f4540191444ff14cfb0536361da9d377a6083f
Pierre-Elie Fauché <pierre-elie@withdouble.com>
2020-04-01 12:58:54 -0400

    ✨ TS linting rules

--- base/typescript.js
+++ base/typescript.js
@@ -10,10 +10,30 @@ module.exports = {
   ],
 
   rules: {
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unused-vars.md
     '@typescript-eslint/no-unused-vars': ['error', {
       args: 'after-used',
       vars: 'all',
       ignoreRestSiblings: true,
+    }],
+
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/strict-boolean-expressions.md
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/brace-style.md
+    '@typescript-eslint/brace-style': ['error', 'stroustrup', {
+      allowSingleLine: true,
+    }],
+
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/explicit-function-return-type.md
+    '@typescript-eslint/explicit-function-return-type': ['warn', {
+      allowExpressions: true,
+    }],
+
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/restrict-template-expressions.md
+    '@typescript-eslint/restrict-template-expressions': ['error', {
+      allowNumber: true,
+      allowBoolean: true,
     }],
   },
 }
