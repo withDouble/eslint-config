801ca1c99c178537e7d603e766c0b02e6c7eb8db
Pierre-Elie Fauché <pierre-elie@withdouble.com>
2020-04-01 13:15:00 -0400

    🐛 TS: disable TS-specific rules in non-ts files

--- base/typescript.js
+++ base/typescript.js
@@ -25,15 +25,25 @@ module.exports = {
       allowSingleLine: true,
     }],
 
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/explicit-function-return-type.md
-    '@typescript-eslint/explicit-function-return-type': ['warn', {
-      allowExpressions: true,
-    }],
-
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/restrict-template-expressions.md
-    '@typescript-eslint/restrict-template-expressions': ['error', {
-      allowNumber: true,
-      allowBoolean: true,
-    }],
+    // Disable for all files, will be enabled by override below
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
   },
+
+  overrides: [{
+    // enable the rule specifically for TypeScript files
+    files: ['*.ts', '*.tsx'],
+    rules: {
+      // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/restrict-template-expressions.md
+      '@typescript-eslint/restrict-template-expressions': ['error', {
+        allowNumber: true,
+        allowBoolean: true,
+      }],
+
+      // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/explicit-function-return-type.md
+      '@typescript-eslint/explicit-function-return-type': ['warn', {
+        allowExpressions: true,
+      }],
+    },
+  }],
 }
