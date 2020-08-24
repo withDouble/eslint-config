292117dd4a79a7fb48e83605deaebcf4ed0bca52
Pierre-Elie Fauché <pierre-elie@withdouble.com>
2020-08-24 18:20:53 -0400

    🔧 rules for new TS config

--- base/typescript-rules.js
+++ base/typescript-rules.js
@@ -13,6 +13,11 @@ module.exports = {
       ignoreRestSiblings: true,
     }],
 
+    '@typescript-eslint/no-unsafe-assignment': 'warn',
+    '@typescript-eslint/no-unsafe-member-access': 'warn',
+    '@typescript-eslint/no-unsafe-return': 'warn',
+    '@typescript-eslint/no-unsafe-call': 'warn',
+
     // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/member-delimiter-style.md
     '@typescript-eslint/member-delimiter-style': ['error', {
       multiline: {
@@ -28,8 +33,6 @@ module.exports = {
     // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/strict-boolean-expressions.md
     '@typescript-eslint/strict-boolean-expressions': 'off',
 
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/ban-ts-ignore.md
-    '@typescript-eslint/ban-ts-ignore': 'warn',
     // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/ban-ts-comment.md
     '@typescript-eslint/ban-ts-comment': 'warn',
 
