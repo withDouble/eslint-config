ed1e9ce5921fd5b59537f1e47593f011f9891f3d
augustinr <augustin.roux@gmail.com>
2023-04-12 17:11:57 -0700

    ✨ Separate common and web config

---
+++ common.js
@@ -0,0 +1,38 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "promise"],
+  extends: ["prettier", "plugin:promise/recommended"],
+  rules: {
+    "prefer-const": "error",
+    camelcase: ["error", { allow: ["_.*Fragment"] }],
+    "no-trailing-spaces": "error",
+    "require-await": "error",
+    "no-debugger": "error",
+    "no-duplicate-imports": "error",
+    "no-console": [
+      "error",
+      {
+        allow: [
+          "info",
+          "debug",
+          "warn",
+          "error",
+          "group",
+          "groupCollapsed",
+          "groupEnd",
+        ],
+      },
+    ],
+
+    // plugin rules
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        varsIgnorePattern: "^_",
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/array-type": ["error", { default: "array" }],
+    "promise/prefer-await-to-then": "error",
+  },
+};
--- web.js
+++ web.js
@@ -1,49 +1,14 @@
+var common = require("./common");
+
 module.exports = {
   parser: "@typescript-eslint/parser",
-  plugins: [
-    "@typescript-eslint",
-    "react-hooks",
-    "promise",
-    "react"
-  ],
-  extends: [
-    "prettier",
-    "plugin:react-hooks/recommended",
-    "plugin:promise/recommended"
-  ],
-  rules: {
-    "prefer-const": "error",
-    "camelcase": ["error", { "allow": ["_.*Fragment"] }],
-    "no-trailing-spaces": "error",
-    "require-await": "error",
-    "no-debugger": "error",
-    "no-duplicate-imports": "error",
-    "no-console": [
-      "error",
-      {
-        "allow": [
-          "info",
-          "debug",
-          "warn",
-          "error",
-          "group",
-          "groupCollapsed",
-          "groupEnd"
-        ]
-      }
-    ],
-
-    // plugin rules
-    "@typescript-eslint/no-unused-vars": ["error", {
-      "varsIgnorePattern": "^_",
-      "argsIgnorePattern": "^_"
-    }],
-    "@typescript-eslint/array-type": ["error", { "default": "array" }],
-    "promise/prefer-await-to-then": "error",
-    "react/jsx-key": ["error", { "checkFragmentShorthand": true }],
+  plugins: ["react-hooks", "react"].concat(common.plugins),
+  extends: ["plugin:react-hooks/recommended"].concat(common.extends),
+  rules: Object.assign(common.rules, {
+    "react/jsx-key": ["error", { checkFragmentShorthand: true }],
     "react/jsx-curly-brace-presence": ["error", "always"],
-  },
+  }),
   env: {
-    "browser": true
-  }
-}
+    browser: true,
+  },
+};
